c36ae8eaa0c2ead6ee04d863400b7a274089e139
Ross Phillips <rphillips-nz@users.noreply.github.com>
2021-10-21 11:01:16 +1300

    Add more logging to injected config

--- src/inject-cloudcannon.config.js
+++ src/inject-cloudcannon.config.js
@@ -1,17 +1,20 @@
 const pluginCloudCannon = require('eleventy-plugin-cloudcannon');
 
+let defaultConfig;
+
+try {
+	defaultConfig = require('./default-eleventy.config.js');
+} catch (e) {
+	console.error('⚠️ Unable to require your config from eleventy-plugin-cloudcannon:', e);
+}
+
 // Used when the 'Manage eleventy-plugin-cloudcannon manually' option is disabled in CloudCannon.
 // Adds this plugin after running the renamed default config file.
 module.exports = function (eleventyConfig, ...args) {
-	let defaultConfig;
-
-	try {
-		defaultConfig = require('./default-eleventy.config.js');
-	} catch {
-		console.error('Not found: ./default-eleventy.config.js');
-	}
-
+	console.log('🔮 Starting automatic eleventy-plugin-cloudcannon');
 	const config = defaultConfig?.apply?.(this, [eleventyConfig, ...args]) ?? {};
+	console.log('☑️ Processed default configuration for eleventy-plugin-cloudcannon');
 	eleventyConfig.addPlugin(pluginCloudCannon, eleventyConfig.cloudcannonOptions ?? config);
+	console.log('☑️ Finished eleventy-plugin-cloudcannon');
 	return config;
 };
